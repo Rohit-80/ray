22c5b61136af00124ae813080a37ce01cadc0d13
Rohit-80 <shrirohitvishwakarma80@gmail.com>
2024-01-15 12:54:27 +0530

    ray pc

---
+++ scripts.js
@@ -0,0 +1,27 @@
+
+
+class obj {
+     constructor(first,second){
+        this.first = first;
+        this.second = second;
+        console.log(first + ' ' + second)
+     }
+
+}
+
+
+
+
+
+let a = new obj('rohit','vishwakarma')
+console.log(a instanceof Object)
+// for( x in arr) {
+//     console.log(x)
+// }
+
+// for( x of arr) {
+//     console.log (x)
+// }
+
+
+// console.log(a)
